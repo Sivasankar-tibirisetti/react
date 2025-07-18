8debea5e88a733263a74a870778ed635d9a7ee14
Sivasankar-tibirisetti <t.shivashankar0153@gmail.com>
2025-07-18 14:52:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+// import Task1 from './Task1/Task1';
+// import Task2 from './Task2/Task2';
+import Task3 from './Task3/Task3'
+import Task4 from './task4/Task4'
+
+function App() {
+  return (
+    <div>
+     {/* <Task1/>  */}
+     {/* <Task2/> */}
+     {/* <Task3/> */}
+     <Task4/>
+    </div>
+  );
+}
+
+export default App;
